a6b61908b0caeab605b37149a5a90be64ec0ad33
nyw <1500920628@qq.com>
2021-03-16 13:38:51 +0800

    1

--- src/components/drawer.vue
+++ src/components/drawer.vue
@@ -3,7 +3,7 @@
     <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">
       点我打开
     </el-button>
-    <el-drawer title="我是标题" :visible.sync="drawer" :direction="direction">
+    <el-drawer title="导航" :visible.sync="drawer" :direction="direction">
       <router-link to="/404">404</router-link>
       <router-link to="/click">点击</router-link>
       <router-link to="/hover">悬浮</router-link>
@@ -31,5 +31,18 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.drawer {
+  .el-drawer__body {
+    display: flex;
+    justify-content: center;
+  }
+  a {
+    flex-basis: 20%;
+    display: inline-block;
+    margin: 10px;
+    color: rgb(31, 169, 224);
+    text-decoration: none;
+  }
+}
 </style>
